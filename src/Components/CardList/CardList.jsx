601f6bee932cda6f2fae4902fcb78acc99daddc4
danielFurtado1 <danielbmfurtado@gmail.com>
2019-09-09 21:05:48 -0300

    starting the app

---
+++ src/Components/CardList/CardList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './CardList.css'
+
+
+export const CardList = (props) => {
+    console.log(props)
+    return <div className='card-list'>{props.children}</div>
+}
