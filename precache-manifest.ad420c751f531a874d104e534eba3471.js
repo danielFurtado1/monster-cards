72b9a8ebc794befcfabfc67eebe22aafefc33c99
danielFurtado1 <danielbmfurtado@gmail.com>
2019-09-13 12:26:55 -0300

    Updates

---
+++ precache-manifest.ad420c751f531a874d104e534eba3471.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ec87b67f923d996db3793e043a8970",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "7045b498f2442c219cef",
+    "url": "/monster-cards/static/css/main.dc4af0e0.chunk.css"
+  },
+  {
+    "revision": "bd411b447e2729f97150",
+    "url": "/monster-cards/static/js/2.0ad485b3.chunk.js"
+  },
+  {
+    "revision": "7045b498f2442c219cef",
+    "url": "/monster-cards/static/js/main.2bebbe89.chunk.js"
+  },
+  {
+    "revision": "55763dc7f4abb36970a5",
+    "url": "/monster-cards/static/js/runtime~main.244f94c2.js"
+  }
+]);
